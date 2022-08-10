4ebcc696ea59b5c501c53778d20d62b7771a9a29
sayali0713 <sayalibhosale772087@gmail.com>
2022-08-10 20:15:37 +0530

    4th commit

---
+++ src/AbstractionDemo/A.java
@@ -0,0 +1,15 @@
+package AbstractionDemo;
+
+public abstract class A {
+	int i;
+	public A(int i) {
+		this.i=i;
+		System.out.println(i);	
+	}
+	
+	public void add() {
+		
+	}
+	
+
+}
---
+++ src/AbstractionDemo/B.java
@@ -0,0 +1,11 @@
+package AbstractionDemo;
+
+public class B extends A{
+	public B() {
+		super(10);
+	}
+
+	public static void main(String[] args) {
+	;
+	}
+}
